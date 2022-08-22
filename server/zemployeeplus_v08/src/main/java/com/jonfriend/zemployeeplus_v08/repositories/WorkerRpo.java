86cd2c58fb5f83a65081e0534f17f13e7bb9a5b4
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-08-22 18:54:40 -0500

    loginReg and workerCRUD fully built and tested in postman.  This push establishes code freeze for java

--- server/zemployeeplus_v08/src/main/java/com/jonfriend/zemployeeplus_v08/repositories/WorkerRpo.java
+++ server/zemployeeplus_v08/src/main/java/com/jonfriend/zemployeeplus_v08/repositories/WorkerRpo.java
@@ -3,6 +3,7 @@ package com.jonfriend.zemployeeplus_v08.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.jonfriend.zemployeeplus_v08.models.WorkerMdl;
@@ -10,5 +11,9 @@ import com.jonfriend.zemployeeplus_v08.models.WorkerMdl;
 
 public interface WorkerRpo extends CrudRepository<WorkerMdl, Long> {
     
-    List<WorkerMdl> findAll();
+	@Query(value="SELECT w.first_name, w.last_name, d.division_name FROM worker w join division d on w.division_id = d.id", nativeQuery=true)
+	List<WorkerMdl> findAllWorkerPlus();
+
+	List<WorkerMdl> findAll();
+
 }
